ea843afc60a11a77f3eee7e6fd25166ec8e10fa7
batimovil <german.figueroa@ort.edu.ar>
2022-05-26 02:14:10 -0300

    form mejorado

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Form.module.css';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-//📨
+
 const Formu = () => (
     <div>
       <Formik
@@ -9,11 +9,11 @@ const Formu = () => (
         validate={values => {
           const errors = {};
           if (!values.email) {
-            errors.email = 'Required';
+            errors.email = 'Requerido';
           } else if (
             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
           ) {
-            errors.email = 'Invalid email address';
+            errors.email = 'Mail invalido';
           }
           return errors;
         }}
@@ -28,18 +28,21 @@ const Formu = () => (
           <Form>
             <div className={styles['columna-1']}>
                 
-                <div> 
+                <div className={styles['inputs-3']}> 
                     <Field type="text" name="name" placeholder='Nombre' />
                     <ErrorMessage name="name" component="div" />    
 
-                    <Field type="email" name="email" placeholder='E-mail' />
-                    <ErrorMessage name="email" component="div" />
+                    <div>
+                        <Field type="email" name="email" placeholder='E-mail' />
+                        <ErrorMessage name="email" component="div" className={styles['requiered-warning']} />
+                    </div>
 
                     <Field type="text" name="subject" placeholder='Asunto' />
                     <ErrorMessage name="subject" component="div" />
+
                 </div>
                 
-                <div>
+                <div className={styles['textarea-div']}>
                     <Field name="message" component="textarea" placeholder='Mensaje'/>
                     <ErrorMessage name="message" component="div" />
                 </div>
@@ -55,47 +58,5 @@ const Formu = () => (
       </Formik>
     </div>
   );
-
-
-// const Form = () => {
-//   return (
-//     <div> 
-
-//         <form>
-//                 <div className={styles['columna-1']}>
-//                     <div>
-
-//                         <div className={styles['name']}>
-//                             <input id="fromName" name="fromName" placeholder='Nombre' type="text"></input>
-
-//                         </div>
-
-//                         <div className={styles['email']}>
-//                             <input id="fromEmail" name="fromEmail" placeholder='E-mail' type="email"></input>
-
-//                         </div>
-
-//                         <div className={styles['asunto']}>
-//                             <input id="fromAsunto" name="fromAsunto" placeholder='Asunto' type="text"></input>
-
-//                         </div>
-
-//                     </div>
-
-//                     <div className={styles['message']}> 
-//                         <textarea id="message" name="message" placeholder='mensaje' ></textarea>
-
-//                     </div>
-//                 </div>
-
-//                 <div className={styles['columna-2']} >
-//                     <p>↪️</p>  
-//                     <input id="send" name="send" type="submit"></input>
-
-//                 </div>
-//         </form>  
-//     </div>
-//   )
-// }
 
 export default Formu
